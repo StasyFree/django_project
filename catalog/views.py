d11249ee248e481c19da6fb02fe17fabf4450239
Анастасия <aburlakova45@gmail.com>
2023-11-30 15:30:01 +0300

    home

--- catalog/views.py
+++ catalog/views.py
@@ -6,10 +6,4 @@ def home(request):
 
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'Имя: {name}\nТелефон: {phone}\nСообщение: {message}')
-
     return render(request, 'main/contacts.html')
